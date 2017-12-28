26cc678d388c1f00003e9549d0dcbdb902034f8f
Mark Snodgrass <msnodgr1@gmail.com>
2017-12-28 16:42:21 -0500

    initial login push that was late sry

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./nav.css";
+
+export const Nav = () =>
+    <ul className="nav nav-tabs">
+        <li className="nav-item">
+            <a className="nav-link" href="test.com">Home</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="test.com">Add Medication</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="test.com">My Medication</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="test.com">My Vitals</a>
+        </li>
+    </ul>
